87676720b68d42ee9c47fce9d0b07f84bf2be3da
charlie-OwO <yeah.charlie@outlook.com>
2025-11-26 19:07:59 +1100

    Major change in GUI node
    I change the pose array source from fake data to real data by subscribe the pose arrays from camera detect node.

--- src/gui_node/launch/gui_node.launch.py
+++ src/gui_node/launch/gui_node.launch.py
@@ -5,7 +5,6 @@ from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
-    # 找到配置文件路径
     rviz_config_file = os.path.join(
         get_package_share_directory('gui_node'),
         'launch',
@@ -13,7 +12,6 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        # 启动你的 gui_node 节点
         Node(
             package='gui_node',
             executable='gui_node',
@@ -21,12 +19,11 @@ def generate_launch_description():
             output='screen'
         ),
         Node(
-            package='os_node',           # 如果 os_node 是单独的包
+            package='os_node',           # 如果 os_node 是单独的包且需要
             executable='os_node',        # 改成实际的可执行文件名
             name='os_node',
             output='screen'
         ),
-        # 启动 RViz 并加载配置
         ExecuteProcess(
             cmd=['rviz2', '-d', rviz_config_file],
             output='screen'
